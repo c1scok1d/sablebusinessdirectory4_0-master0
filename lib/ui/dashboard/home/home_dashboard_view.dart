92fca497842984c17961bc678a01ea9bfdf92138
c1scok1d <rchatman@macinternetservices.com>
2021-12-19 14:33:48 -0600

    working on permissions/geofencing

--- lib/ui/dashboard/home/home_dashboard_view.dart
+++ lib/ui/dashboard/home/home_dashboard_view.dart
@@ -266,7 +266,6 @@ class _HomeDashboardViewWidgetState extends State<HomeDashboardViewWidget> {
     print('HOME NOW');
 
     initPlatformState();
-    //initializeGeofence();
     return MultiProvider(
         providers: <SingleChildWidget>[
           ChangeNotifierProvider<BlogProvider>(
@@ -753,12 +752,14 @@ class _HomeDashboardViewWidgetState extends State<HomeDashboardViewWidget> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    if (hasAlreadyListened) return;
+    if (hasAlreadyListened)
+      return;
     print('initPlatform state');
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
-    if (!mounted) return;
+    if (!mounted)
+      return;
     geo.Geofence.initialize();
     geo.Geofence.requestPermissions();
     final SharedPreferences sharedPreferences =
